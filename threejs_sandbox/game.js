fbd11726194717bab1713539a9d63d3a8bdf49b1
leandrofahur <leandrofahur@gmail.com>
2020-06-26 02:50:45 -0400

    simple modified example

---
+++ threejs_sandbox/game.js
@@ -0,0 +1,34 @@
+class Game {
+  constructor() {
+    
+    this.t = 0.0;
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    const geometry = new THREE.CircleGeometry(1,360);
+    const material = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    this.camera.position.z = 5;
+    this.animate();
+  }
+
+  animate() {
+    const game = this;
+    requestAnimationFrame(()=>{
+      game.animate();
+    });
+
+    this.cube.position.x = Math.sin(this.t);
+    this.cube.position.y = Math.cos(this.t);
+    this.t += 2*Math.PI/360;
+
+	  this.renderer.render(this.scene, this.camera);
+  }
+}
